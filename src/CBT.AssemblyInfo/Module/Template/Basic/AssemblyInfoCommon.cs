d02315edc68bef2a3b7d03eba2bfafd885339c46
joshking <joshking@JOSHKING-MAIN>
2016-05-23 18:15:09 -0700

    initial add of assembly information generation module

---
+++ src/CBT.AssemblyInfo/Module/Template/Basic/AssemblyInfoCommon.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="%AssemblyInfoTemplateName%" company="%AssemblyInfoCompanyName%">
+// %AssemblyInfoCopyright%
+// </copyright>
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// The following assembly information is common to all product assemblies.
+// If you get compiler errors CS0579, "Duplicate '<attributename>' attribute", check your
+// Properties\AssemblyInfo.cs file and remove any lines duplicating the ones below.
+[assembly: AssemblyCompany("%AssemblyInfoCompanyName%")]
+[assembly: AssemblyProduct("%AssemblyInfoProductName%")]
+[assembly: AssemblyCopyright("%AssemblyInfoCopyright%")]
+[assembly: AssemblyTrademark("%AssemblyInfoTrademark%")]
+[assembly: AssemblyCulture("%AssemblyInfoCulture%")]
+[assembly: AssemblyConfiguration("%AssemblyInfoConfiguration%")]
+[assembly: CLSCompliant(%AssemblyInfoCLSCompliant%)]
+[assembly: ComVisible(%AssemblyInfoComVisible%)]
+
