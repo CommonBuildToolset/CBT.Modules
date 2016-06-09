acfdc047bbcad3d5b4178900294a29a4744e169b
Jeff Kluge <josemesona@hotmail.com>
2016-06-09 08:54:21 -0700

    Fix up package creation

--- src/CBT.DotNetFx/packall.cmd
+++ src/CBT.DotNetFx/packall.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-SET "NETFXTAG=-beta1"
+SET "NETFXTAG=-beta01"
 IF "%ProgramFiles(x86)%" NEQ "" (
 	SET "REFERENCEASSEMBLYROOT=%ProgramFiles(x86)%\Reference Assemblies"
 ) ELSE (
@@ -25,6 +25,6 @@ FOR %%A IN (4.0.0,4.5.0,4.5.1,4.5.2,4.6.0,4.6.1) DO (
 		ECHO .NET Framework reference assemblies were not found at !ROOT!.  Ensure the .NET Framework SDK is installed for this version and try again.
 	) ELSE (
 		ECHO Version %%A%NETFXTAG% - !DisplayName! - !DisplayVersion! - "!ROOT!"
-		NuGet PACK "%~dp0CBT.DotNetFx.nuspec" -Properties "Version=%%A%NETFXTAG%;DisplayName=!DisplayName!;Root=!ROOT!" -NoPackageAnalysis -NoDefaultExcludes -O "%~dp0.."
+		NuGet PACK "%~dp0CBT.DotNetFx.nuspec" -Properties "Version=%%A%NETFXTAG%;DisplayName=!DisplayName!;DisplayVersion=!DisplayVersion!;Root=!ROOT!" -NoPackageAnalysis -NoDefaultExcludes -O "%~dp0.."
 	)
 )
