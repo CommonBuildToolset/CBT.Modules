d0a5b65ef3a274e0d21bba08e30993cb57d40d12
Jeff Kluge <josemesona@hotmail.com>
2016-06-10 12:43:34 -0700

    Fix a bug where package restore is attempted in Visual Studiow when project.json

--- src/packall.cmd
+++ src/packall.cmd
@@ -1,4 +1,6 @@
 @ECHO OFF
+msbuild /v:m /m /nologo CBT.NuGet\CBT.NuGet.csproj /t:Rebuild
+
 FOR /F "usebackq" %%A IN (`dir %~dp0*.nuspec /s /b ^| findstr /v /i "CBT.DotNetFx"`) DO (
 	"NuGet.exe" pack "%%A" -OutputDirectory %~dp0
 )
