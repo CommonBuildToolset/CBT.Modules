d0188129a33d5b07769eb0f2390ad5a5d91cdb79
kingerja <kingerja@hotmail.com>
2016-07-03 09:14:27 -0700

    AssemblyInfo and AssemblyVersioning module initial commit
    * initial add of assembly information generation module
    
    * ad versioing nuspec
    
    * refactor targets to keep assemblyinfo stuff in managed target
    
    * clean up assemblyinfo module and add versioning module
    
    * Update assemblyVersioning and AssemblyInfo modules to simplify basic logic and add support for vbproj and vcxproj

---
+++ src/CBT.AssemblyInfo/Module/Template/Basic/AssemblyInfoCommon.vb
@@ -0,0 +1,18 @@
+﻿' <copyright>%AssemblyInfoCopyright%</copyright>
+
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' The following assembly information is common to all product assemblies.
+' If you get compiler errors CS0579, "Duplicate '<attributename>' attribute", check your 
+' My Project\AssemblyInfo.vb file and remove any lines duplicating the ones below.
+<Assembly: AssemblyCompany("%AssemblyInfoCompanyName%")> 
+<Assembly: AssemblyProduct("%AssemblyInfoProductName%")>
+<Assembly: AssemblyCopyright("%AssemblyInfoCopyright%")>
+<Assembly: AssemblyTrademark("%AssemblyInfoTrademark%")>
+<Assembly: AssemblyCulture("%AssemblyInfoCulture%")>
+<Assembly: AssemblyConfiguration("%AssemblyInfoConfiguration%")>
+<Assembly: CLSCompliant(%AssemblyInfoCLSCompliant%)>
+<Assembly: ComVisible(%AssemblyInfoComVisible%)>
+
