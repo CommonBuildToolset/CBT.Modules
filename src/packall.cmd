f67cb4a5da716ead47e0892d85628ea859ee5fcb
Jeff Kluge <josemesona@hotmail.com>
2016-07-26 08:54:25 -0700

    Adopt CBT

--- src/packall.cmd
+++ src/packall.cmd
@@ -1,6 +1,12 @@
 @ECHO OFF
-msbuild /v:m /m /nologo CBT.NuGet\CBT.NuGet.csproj /t:Rebuild
+msbuild /v:m /m /nologo dirs.proj /t:Rebuild
 
 FOR /F "usebackq" %%A IN (`dir %~dp0*.nuspec /s /b ^| findstr /v /i "CBT.DotNetFx"`) DO (
-	"NuGet.exe" pack "%%A" -OutputDirectory %~dp0
+	CALL :Pack "%%A"
 )
+
+GOTO :EOF
+
+:Pack
+
+"NuGet.exe" pack "%~1" -OutputDirectory %~dp0 -Properties "BinDir=%~dp0..\bin\Debug\AnyCPU\%~n1"
