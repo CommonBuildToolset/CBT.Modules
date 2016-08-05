2438540028dcc1c3125c1cabe1d74c9b79914027
Jeff Kluge <jeffkl@microsoft.com>
2016-08-05 10:58:39 -0700

    Fix issue where CBT.NuGet.dll wasn't included in package because BinDir contained .."

--- src/packall.cmd
+++ src/packall.cmd
@@ -2,11 +2,11 @@
 msbuild /v:m /m /nologo dirs.proj /t:Rebuild
 
 FOR /F "usebackq" %%A IN (`dir %~dp0*.nuspec /s /b ^| findstr /v /i "CBT.DotNetFx"`) DO (
-	CALL :Pack "%%A"
+	CALL :Pack "%%A" "%~dp0.."
 )
 
 GOTO :EOF
 
 :Pack
 
-"NuGet.exe" pack "%~1" -OutputDirectory %~dp0 -Properties "BinDir=%~dp0..\bin\Debug\AnyCPU\%~n1"
+"NuGet.exe" pack "%~1" -OutputDirectory %~dp0 -Properties "BinDir=%~f2\bin\Debug\AnyCPU\%~n1"
