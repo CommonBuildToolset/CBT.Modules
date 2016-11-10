0c865b3cb01eb99b2f40f5484a7c0676f3ce285a
Jeff Kluge <jeffkl@microsoft.com>
2016-11-10 13:27:39 -0800

    Adopt NuProj projects Deprecate usage of CBT.AssemblyInfo in favor of Nerdbank.GitVersioning CBT.DotNetFx is still a batch file for now

--- src/CBT.NuGet/Properties/AssemblyInfo.cs
+++ src/CBT.NuGet/Properties/AssemblyInfo.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Common Build Toolset (CBT)")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyDescription("MSBuild task library for NuGet actions")]
+[assembly: AssemblyProduct("Common Build Toolset (CBT)")]
+[assembly: AssemblyTitle("CBT.NuGet")]
+[assembly: AssemblyTrademark("")]
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
