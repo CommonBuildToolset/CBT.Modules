0504928c0645fc1addfc4db293d6a155d5086da0
Josh Kingery <joshking@microsoft.com>
2017-06-06 19:35:28 -0700

    Disable globalpackagerestore if nugetpackagerestore is disabled

--- src/CBT.NuGet/Tasks/NuGetRestore.cs
+++ src/CBT.NuGet/Tasks/NuGetRestore.cs
@@ -49,8 +49,7 @@ namespace CBT.NuGet.Tasks
             // CBTModulesRestored must be set so on it's evaluation it knows to import the generated module imports so it evaluates the proper full closure of the project.
             EnvironmentVariables = new[] {
                 $"CBTEnablePackageRestore=false",
-                $"CBTModulesRestored=true",
-                $"CBTEnableGlobalPackageRestore=false"
+                $"CBTModulesRestored=true"
             };
             // If packages directory was not specified, just execute the restore
             //
