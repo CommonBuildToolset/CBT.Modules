947957ef4e476e68f2b4cd78b65f86ff18299081
kingerja <kingerja@hotmail.com>
2017-06-29 16:51:20 -0700

    Support NuGetPath for non packages.config (#143)
    * Fix version match
    
    * Generate NuGetPath_ properties from packages.config, project.json, packagereference projects.
    Swtich to using Length for unique per project instead of hashcode.  Possible but rare chance of collision but solves issue with inconsistent hash codes for now.

---
+++ src/CBT.NuGet/Internal/NuGetPackagesConfigParser.cs
@@ -0,0 +1,53 @@
+﻿using NuGet.Packaging;
+using NuGet.Packaging.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using NuGet.ProjectManagement;
+
+
+namespace CBT.NuGet.Internal
+{
+    /// <summary>
+    /// Represents a class that can parse a NuGet packages.config file.
+    /// </summary>
+    internal sealed class NuGetPackagesConfigParser : INuGetPackageConfigParser
+    {
+        public IEnumerable<PackageIdentityWithPath> GetPackages(string packagesPath, string packageConfigPath, PackageRestoreData packageRestoreData)
+        {
+            if (packageRestoreData != null &&
+                !packageRestoreData.RestoreProjectStyle.Equals("Unknown",
+                    StringComparison.InvariantCultureIgnoreCase))
+            {
+                yield break;
+            }
+            PackagePathResolver packagePathResolver = new PackagePathResolver(packagesPath);
+
+            // If a *proj was passed in but it is really a packages.config project then set the packages.config file.
+            if (packageRestoreData != null && packageRestoreData.RestoreProjectStyle.Equals("Unknown", StringComparison.OrdinalIgnoreCase))
+            {
+                var tmpPackageConfigPath = Path.Combine(Path.GetDirectoryName(packageConfigPath ?? string.Empty) ?? string.Empty, "packages.config");
+                if (File.Exists(tmpPackageConfigPath))
+                {
+                    packageConfigPath = tmpPackageConfigPath;
+                }
+            }
+
+            if (!packageConfigPath.EndsWith(Constants.PackageReferenceFile, StringComparison.OrdinalIgnoreCase))
+            {
+                yield break;
+            }
+
+            XDocument document = XDocument.Load(packageConfigPath);
+
+            PackagesConfigReader packagesConfigReader = new PackagesConfigReader(document);
+
+            foreach (PackageIdentity item in packagesConfigReader.GetPackages(allowDuplicatePackageIds: true).Select(i => i.PackageIdentity))
+            {
+                yield return new PackageIdentityWithPath(item, packagePathResolver.GetPackageDirectoryName(item), packagePathResolver.GetInstallPath(item));
+            }
+        }
+    }
+}
---
+++ src/CBT.NuGet/Internal/PackageRestoreData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace CBT.NuGet.Internal
+{
+    internal class RestorePackage
+    {
+        public RestorePackage(string id, string version)
+        {
+            Id = id;
+            Version = string.IsNullOrWhiteSpace(version)? "0.0.0" : version;
+        }
+        public string Id { get; }
+        public string Version { get; }
+    }
+
+    internal class PackageRestoreData
+    {
+        public string RestoreOutputAbsolutePath { get; set; }
+
+        // Using custom RestorePackage item since NuGetVersion requires a custom serializer and reasoning about the formation of the nuget version can be tricky for all the potential ways versions are done.
+        public IList<RestorePackage> PackageImportOrder { get; set; }
+
+        // Type of projectStyle that nuget is restoring PackageReference, ProjectJson, Unknown 
+        public string RestoreProjectStyle { get; set; }
+
+        // Path of project.json if restoreprojectstyle is projectjson
+        public string ProjectJsonPath { get; set; }
+    }
+}
