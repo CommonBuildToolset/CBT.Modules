0960936ebd8af1b6a6993b693695c6a1cbf143a0
Jeff Kluge <jeffkl@microsoft.com>
2017-07-20 14:35:23 -0700

    Load CBT.NuGet.dll into memory as a byte[] to avoid file locking (#153)
    * Load CBT.NuGet.dll into memory as a byte[] to avoid file locking
    
    Leave a breadcrumb in the AppDomain as to where CBT.NUGet.dll is
    Updated the AssemblyResolve event handler to use the new search paths
    
    * Load dependent assemblies as a byte[] to avoid locking
    
    * Update CBT.NuGet.AggregatePackage functionality
    
    * Update CBT.NuGet.GlobalPackages

--- src/CBT.NuGet/Tasks/GenerateNugetProperties.cs
+++ src/CBT.NuGet/Tasks/GenerateNugetProperties.cs
@@ -1,11 +1,13 @@
 ﻿using CBT.NuGet.Internal;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
-using Newtonsoft.Json;
 
 namespace CBT.NuGet.Tasks
 {
@@ -17,10 +19,44 @@ namespace CBT.NuGet.Tasks
     public sealed class GenerateNuGetProperties : Task
     {
         /// <summary>
-        /// Gets or sets the packages.config or project.json file to be parsed by this command.
+        /// Stores a list of assembly search paths where dependencies should be searched for.
         /// </summary>
-        [Required]
-        public string PackageRestoreFile { get; set; }
+        private readonly ICollection<string> _assemblySearchPaths = new List<string>();
+
+        /// <summary>
+        /// Stores a list of loaded assemblies in the event that the same assembly is requested multiple times.
+        /// </summary>
+        private readonly IDictionary<AssemblyName, Assembly> _loadedAssemblies = new Dictionary<AssemblyName, Assembly>();
+
+        private readonly CBTTaskLogHelper _log;
+
+        public GenerateNuGetProperties()
+        {
+            _log = new CBTTaskLogHelper(this);
+
+            string executingAssemblyLocation = Assembly.GetExecutingAssembly().Location;
+
+            if (!String.IsNullOrWhiteSpace(executingAssemblyLocation))
+            {
+                // When loading an assembly from a byte[], the Assembly.Location is not set so it shouldn't be considered
+                //
+                _assemblySearchPaths.Add(Path.GetDirectoryName(executingAssemblyLocation));
+            }
+
+            if (AppDomain.CurrentDomain.GetData("CBT_NUGET_ASSEMBLY_PATH") != null)
+            {
+                // CBT.NuGet.props currently sets this value so we can determine where CBT.NuGet.dll is since its loaded as a byte[]
+                //
+                _assemblySearchPaths.Add(Path.GetDirectoryName(AppDomain.CurrentDomain.GetData("CBT_NUGET_ASSEMBLY_PATH").ToString()));
+            }
+
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        /// <summary>
+        /// Gets or sets the assets file from the nuget restore.
+        /// </summary>
+        public string AssetsFile { get; set; }
 
         /// <summary>
         /// Gets or sets the list of file inputs that should be considered for incrementality if the PropsFile exist and is older then the inputs this is considered a no-op.
@@ -29,16 +65,15 @@ namespace CBT.NuGet.Tasks
         public string[] Inputs { get; set; }
 
         /// <summary>
-        /// Gets or sets the full path of the props file that is written.
+        /// Gets or sets the NuGetPackagesPath.
         /// </summary>
-        [Required]
-        public string PropsFile { get; set; }
+        public string NuGetPackagesPath { get; set; }
 
         /// <summary>
-        /// Gets or sets the property name prefix for the version variables. Properties are formed as ($(PropertyVersionNamePrefix_)+PackageID.Replace('.','_')).
+        /// Gets or sets the packages.config or project.json file to be parsed by this command.
         /// </summary>
         [Required]
-        public string PropertyVersionNamePrefix { get; set; }
+        public string PackageRestoreFile { get; set; }
 
         /// <summary>
         /// Gets or sets the property name prefix for the path variables. Properties are formed as ($(PropertyPathNamePrefix_)+PackageID.Replace('.','_')).
@@ -53,27 +88,19 @@ namespace CBT.NuGet.Tasks
         public string PropertyPathValuePrefix { get; set; }
 
         /// <summary>
-        /// Gets or sets the NuGetPackagesPath.
+        /// Gets or sets the property name prefix for the version variables. Properties are formed as ($(PropertyVersionNamePrefix_)+PackageID.Replace('.','_')).
         /// </summary>
-        public string NuGetPackagesPath { get; set; }
+        [Required]
+        public string PropertyVersionNamePrefix { get; set; }
 
         /// <summary>
-        /// Gets or sets the assets file from the nuget restore.
+        /// Gets or sets the full path of the props file that is written.
         /// </summary>
-        public string AssetsFile { get; set; }
-
-        private readonly CBTTaskLogHelper _log;
-
-
-        public GenerateNuGetProperties()
-        {
-            _log = new CBTTaskLogHelper(this);
-        }
+        [Required]
+        public string PropsFile { get; set; }
 
         public override bool Execute()
         {
-            // Tell appdomain to load nuget references and newtonsoft.json from current directory.
-            UpdateAssemblySearchPaths();
             Log.LogMessage(MessageImportance.Low, "Generating MSBuild property file '{0}' for NuGet packages", PropsFile);
             NuGetPropertyGenerator nuGetPropertyGenerator = new NuGetPropertyGenerator(_log, PackageRestoreFile);
             nuGetPropertyGenerator.Generate(PropsFile, PropertyVersionNamePrefix, PropertyPathNamePrefix, PropertyPathValuePrefix, NuGetPackagesPath, GetPackageRestoreData());
@@ -138,17 +165,31 @@ namespace CBT.NuGet.Tasks
             return JsonConvert.DeserializeObject<PackageRestoreData>(File.ReadAllText(AssetsFile));
         }
 
-        private void UpdateAssemblySearchPaths()
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
-            AppDomain.CurrentDomain.AssemblyResolve += LoadFromSameFolder;
-        }
-        static Assembly LoadFromSameFolder(object sender, ResolveEventArgs args)
-        {
-            string folderPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string assemblyPath = Path.Combine(folderPath, new AssemblyName(args.Name).Name + ".dll");
-            if (!File.Exists(assemblyPath)) return null;
-            Assembly assembly = Assembly.LoadFrom(assemblyPath);
-            return assembly;
+            AssemblyName assemblyName = new AssemblyName(args.Name);
+
+            // Return the assembly if its already been loaded
+            //
+            if (_loadedAssemblies.ContainsKey(assemblyName))
+            {
+                return _loadedAssemblies[assemblyName];
+            }
+
+            // Return the first assembly search path that contains the requested assembly
+            //
+            string assemblyPath = _assemblySearchPaths.Select(i => Path.Combine(i, $"{assemblyName.Name}.dll")).FirstOrDefault(File.Exists);
+
+            if (assemblyPath != null)
+            {
+                // Load the assembly and keep it in the list of loaded assemblies
+                //
+                _loadedAssemblies[assemblyName] = Assembly.Load(File.ReadAllBytes(assemblyPath));
+
+                return _loadedAssemblies[assemblyName];
+            }
+
+            return null;
         }
     }
 }
