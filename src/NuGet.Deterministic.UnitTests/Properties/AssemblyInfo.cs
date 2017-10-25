71fb4e4037330863cc0d1461ab663a7a44e68252
Jeff Kluge <jeffkl@microsoft.com>
2017-10-25 00:32:24 -0700

    WIP

---
+++ src/NuGet.Deterministic.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGet.Deterministic.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NuGet.Deterministic.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2277e70a-d81c-4a35-bf96-e8cd358bc864")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/NuGet.Deterministic.UnitTests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NuGet.Commands;
+using NuGet.Common;
+using NuGet.LibraryModel;
+using NuGet.ProjectModel;
+
+namespace NuGet.Deterministic.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            LockFile lockFile = LockFileUtilities.GetLockFile(@"D:\CommonBuildToolset\CloudBuild.Modules\src\CBT.CloudBuild\obj\project.assets.json", new NullLogger());
+
+            foreach (LockFileLibrary lockFileLibrary in lockFile.Libraries.Where(p => p.Type.Equals("package")))
+            {
+                LibraryDependency libraryDependency = lockFile.PackageSpec.TargetFrameworks.Select(i => i.Dependencies.FirstOrDefault(x => x.Name.Equals(lockFileLibrary.Name))).FirstOrDefault();
+
+                if (libraryDependency != null)
+                {
+                    string privateAssets = libraryDependency.SuppressParent.ToString();
+
+                    string includeAssets = libraryDependency.IncludeType.ToString();
+                }
+            }
+        }
+    }
+}
