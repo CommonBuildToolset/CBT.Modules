390a9ea3f933114d10f9a5bca076c0fa0628bed5
Jeff Kluge <jeffkl@microsoft.com>
2018-04-06 11:00:30 -0700

    Fix up-to-date check in ImportBuildPackages (#234)
    * Fix up-to-date check in ImportBuildPackages
    
    * Reuse NuGetRetore.IsUpToDate() because it has more logging
    * Remove BeforeRun logic because it was only looking at file existence and was redundant anyway
    * Added a lot more logging so its easier to diagnose
    * Add unit tests

--- src/CBT.UnitTests.Common/TestBase.cs
+++ src/CBT.UnitTests.Common/TestBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace CBT.UnitTests.Common
@@ -14,6 +15,25 @@ namespace CBT.UnitTests.Common
                 Directory.CreateDirectory(_testRootPath);
                 return _testRootPath;
             }
+        }
+
+        public string CreateDirectory(string name, IDictionary<string, string> files = null)
+        {
+            DirectoryInfo directory = Directory.CreateDirectory(Path.Combine(TestRootPath, name));
+
+            if (files != null)
+            {
+                foreach (KeyValuePair<string, string> file in files)
+                {
+                    FileInfo fileInfo = new FileInfo(Path.Combine(directory.FullName, file.Key));
+
+                    fileInfo.Directory.Create();
+
+                    File.WriteAllText(fileInfo.FullName, file.Value);
+                }
+            }
+
+            return directory.FullName;
         }
 
         public void Dispose()
