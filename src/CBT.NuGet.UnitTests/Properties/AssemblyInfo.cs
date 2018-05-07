1e3aaf86a5199f09a101bfe6ddacc536c64749e9
Jeff Kluge <jeffkl@microsoft.com>
2018-05-07 09:40:27 -0700

    wip

--- src/CBT.NuGet.UnitTests/Properties/AssemblyInfo.cs
+++ src/CBT.NuGet.UnitTests/Properties/AssemblyInfo.cs
@@ -1,18 +1,1 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCompany("Common Build Toolset (CBT)")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCopyright("Copyright © 2016")]
-[assembly: AssemblyDescription("Unit tests for CBT.NuGet.dll")]
-[assembly: AssemblyProduct("Common Build Toolset (CBT)")]
-[assembly: AssemblyTitle("CBT.NuGet.UnitTests")]
-[assembly: AssemblyTrademark("")]
-[assembly: CLSCompliant(false)]
-[assembly: ComVisible(false)]
+﻿
