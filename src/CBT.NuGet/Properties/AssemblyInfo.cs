b7e32a9f09328d5c03fbb6d3cf05e2a0937d5800
Jeff Kluge <jeffkl@microsoft.com>
2018-05-07 11:49:26 -0700

    Convert projects back to stock and away from NuProj (#247)
    * Change everything to new SDK-style projects
    * Move to PackageReference
    * Remove references to nuproj
    * Remove dirs.proj
    * Remove CBT
    * Add Directory.Build.rsp
    * Update appveyor.yml
    * Move solution file

--- src/CBT.NuGet/Properties/AssemblyInfo.cs
+++ src/CBT.NuGet/Properties/AssemblyInfo.cs
@@ -1,21 +1,4 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCompany("Common Build Toolset (CBT)")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCopyright("Copyright © 2016")]
-[assembly: AssemblyDescription("MSBuild task library for NuGet actions")]
-[assembly: AssemblyProduct("Common Build Toolset (CBT)")]
-[assembly: AssemblyTitle("CBT.NuGet")]
-[assembly: AssemblyTrademark("")]
-[assembly: CLSCompliant(false)]
-[assembly: ComVisible(false)]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("CBT.NuGet.UnitTests")]
 [assembly: InternalsVisibleTo("CBT.NuGet.AggregatePackage.UnitTests")]
--- src/NuGet.Deterministic/Properties/AssemblyInfo.cs
+++ src/NuGet.Deterministic/Properties/AssemblyInfo.cs
@@ -1,20 +1,3 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCompany("Common Build Toolset (CBT)")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCopyright("Copyright © 2016")]
-[assembly: AssemblyDescription("MSBuild task library for generating .props with nuget versions.")]
-[assembly: AssemblyProduct("Common Build Toolset (CBT)")]
-[assembly: AssemblyTitle("CBT.NuGet.Deterministic")]
-[assembly: AssemblyTrademark("")]
-[assembly: CLSCompliant(false)]
-[assembly: ComVisible(false)]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("NuGet.Deterministic.UnitTests")]
